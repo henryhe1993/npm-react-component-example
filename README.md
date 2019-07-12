eeb478accf3a8095f5d7297b9d0c067a8144b549
htl <hetianlun@supcon.com>
2019-07-12 15:14:13 +0800

    htl

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # npm-react-component-example
 npm-react-component-example
+
+
+
+npm run build
+
+npm publish
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ export default class extends React.Component {
   render() {
     return (
       <div>
-        <span>react npm publish</span>
+        <span>react npm publish add some npmignore</span>
         <Ul>
           { Array(20).fill(100).map((item, index) => <Li key={index} text={this.props.text}></Li>) }
         </Ul>
