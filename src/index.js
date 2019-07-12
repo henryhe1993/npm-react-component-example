1c07317644d044b4f72cfa26948798a84fabee4c
htl <hetianlun@supcon.com>
2019-07-12 14:53:08 +0800

    htl

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Ul from './ul';
+import Li from './li';
+
+export default class extends React.Component {
+  render() {
+    return (
+      <div>
+        <span>react npm publish</span>
+        <Ul>
+          { Array(20).fill(100).map((item, index) => <Li key={index} text={this.props.text}></Li>) }
+        </Ul>
+      </div>
+    );
+  }
+}
